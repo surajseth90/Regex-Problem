a91d69df58395694bd9cf07ba60240d08ecd0d29
surajseth90 <surajseth90@gmail.com>
2021-04-27 11:53:30 +0530

    Refactor the code for Use case 5 of email

--- src/Email.java
+++ src/Email.java
@@ -4,14 +4,14 @@ public class Email {
 
 	public static void validEmail() {
 		Scanner scanner = new Scanner(System.in);
-		String emailCondition = "[a-z]{3,}[a-z\\.\\+\\-\\_]{4,}?[@](bridgelabz)(.co)(.in)?";
+		String emailCondition = "[a-z]{3,}([a-z\\.\\+\\-\\_]{4,})?[@](bridgelabz)(.co)([a-z.]{3})?";
 		System.out.println("Please enter your valid email ID :");
 		String valid = scanner.nextLine();
 		CheckCondition.checkCondition(valid,emailCondition);
 		if(CheckCondition.checkCondition(valid,emailCondition))
 			System.out.println();
 		else {
-			System.out.println("Invalid Input : Please Enter in a proper format eg. xyz@bridgelabz.co ");
+			System.out.println("Invalid Input : Please Enter in a proper format eg. abc.xyz@bridgelabz.co.in (.xyz and .in are optional) ");
 			Email.validEmail();
 		}
 	}
